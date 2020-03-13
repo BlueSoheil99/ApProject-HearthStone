8c4fdf2814f604600148f3bff3107825aa69cabb
BlueSoheil99 <Soheil.k.78@gmail.com>
2020-03-14 01:38:04 +0330

    just made the outline of Carts and Hero class

---
+++ src/project01/CartsAndHeros/Hero.java
@@ -0,0 +1,24 @@
+package project01.CartsAndHeros;
+
+public abstract class Hero {
+     int HP = 30 ;
+//    private HeroPower[] heroPowers={one , two , three}
+//    private HeroPower heroPower = null;
+    private Hero(Carts[] heroPower){}
+//    Hero Mage = new Hero (HeroPower heroPower);
+//    Hero Rogue= new Hero(HeroPower heroPower);
+//    Hero Warlock = new Hero(HeroPower heroPower);
+
+}
+//class Mage extends Hero{
+//
+//}
+//
+//class Rogue extends Hero{
+//
+//}
+//
+//class Warlock extends Hero{
+//    HP =35;
+//
+//}
